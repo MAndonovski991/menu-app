5ce039780cdc68b344da24dbdad118bf5f3308f5
Martin Andonovski <m.andonovski991@gmail.com>
2022-07-15 10:10:05 +0200

    meta

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import './App.css';
 import Home from './components/Home/Home';
 
+import React,{useEffect} from 'react'
+
 function App() {
+
+  useEffect(() => {
+    const meta = document.createElement("meta")
+
+    meta.setAttribute("http-equiv", "Content-Security-Policy")
+    meta.setAttribute("content", "upgrade-insecure-requests")
+
+    document.head.appendChild(meta)
+  }, [])
+
   return (
     <div className="App">
       <Home />
