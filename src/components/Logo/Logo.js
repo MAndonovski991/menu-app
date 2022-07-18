88561bf6eae36a4307fff18d1f8cff0931ebf3b3
Martin Andonovski <m.andonovski991@gmail.com>
2022-07-18 22:41:48 +0200

    cdn

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,7 +2,7 @@ import "./Logo.css";
 
 function Logo() {
     return(
-        <img className="logo" src="/images/logo.png" alt="choco-house-logo" />
+        <img className="logo" src="https://res.cloudinary.com/martinscloud/image/upload/v1658176729/choco-house/logo.png" alt="choco-house-logo" />
     )
 }
 
